805e99f3f61e24c2b1e7e70dcb9f86b692e8f730
Vladimir <myweb11@mail.ru>
2024-01-10 22:34:33 +0300

    add sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,16 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CostController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/login', [UserController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')
+    ->post('/logout', [UserController::class, 'logout']);
 
-Route::resource('categories', CategoryController::class);
-Route::resource('costs', CostController::class);
+Route::middleware('auth:sanctum')
+    ->resource('categories', CategoryController::class);
+
+Route::middleware('auth:sanctum')
+    ->resource('costs', CostController::class);
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Family;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
