35514c850b6e6f20056954f470dc3d0264d6c6c3
Vladimir <myweb11@mail.ru>
2024-01-23 21:44:30 +0300

    change title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>MyFinance</title>
     </head>
     <body>
         <div class="navbar-fixed">
