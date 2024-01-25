569c869a45f8699bec4cb8f0cf113765e124d72d
Vladimir <myweb11@mail.ru>
2024-01-25 21:30:38 +0300

    test autofocus cost field

--- resources/views/livewire/cost-create.blade.php
+++ resources/views/livewire/cost-create.blade.php
@@ -5,7 +5,7 @@
                 <div class="col">
                     <div class="input-field">
                         <i class="material-icons prefix">attach_money</i>
-                        <input required id="cost-field" type="number" class="center-align price-field" wire:model="price" inputmode="numeric">
+                        <input required type="number" class="center-align price-field" wire:model="price" inputmode="numeric">
                     </div>
                 </div>
                 <div class="col">
@@ -56,38 +56,6 @@
             elem[0].addEventListener('change', function(event) {
                 $wire.$set('date', event.target.value)
             })
-
-            let costField = document.getElementById('cost-field')
-
-            function focusAndOpenKeyboard(el, timeout) {
-                if(!timeout) {
-                    timeout = 100;
-                }
-                if(el) {
-                    // Align temp input element approximately where the input element is
-                    // so the cursor doesn't jump around
-                    var __tempEl__ = document.createElement('input');
-                    __tempEl__.style.position = 'absolute';
-                    __tempEl__.style.top = (el.offsetTop + 7) + 'px';
-                    __tempEl__.style.left = el.offsetLeft + 'px';
-                    __tempEl__.style.height = 0;
-                    __tempEl__.style.opacity = 0;
-                    // Put this temp element as a child of the page <body> and focus on it
-                    document.body.appendChild(__tempEl__);
-                    __tempEl__.focus();
-
-                    // The keyboard is open. Now do a delayed focus on the target element
-                    setTimeout(function() {
-                        el.focus();
-                        el.click();
-                        // Remove the temp element
-                        document.body.removeChild(__tempEl__);
-                    }, timeout);
-                }
-            }
-
-
-            focusAndOpenKeyboard(costField, 1000)
         </script>
     @endscript
 </div>
