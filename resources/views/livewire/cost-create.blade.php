0ee0f6c96a5db7ce44a4a488de1f76af79be5bd2
Vladimir <myweb11@mail.ru>
2024-01-25 21:25:23 +0300

    test autofocus cost field

--- resources/views/livewire/cost-create.blade.php
+++ resources/views/livewire/cost-create.blade.php
@@ -59,19 +59,35 @@
 
             let costField = document.getElementById('cost-field')
 
-            function openIosKeyboard(el) {
-                const input = document.createElement("input");
-                el.setAttribute("type", "text");
-                el.setAttribute("style", "position: fixed; top: -100px; left: -100px;");
-                document.body.appendChild(el);
-                el.focus();
-                // it's safe to remove the fake input after a 30s timeout
-                setTimeout(() => {
-                    document.body.removeChild(el);
-                }, 30 * 1000);
+            function focusAndOpenKeyboard(el, timeout) {
+                if(!timeout) {
+                    timeout = 100;
+                }
+                if(el) {
+                    // Align temp input element approximately where the input element is
+                    // so the cursor doesn't jump around
+                    var __tempEl__ = document.createElement('input');
+                    __tempEl__.style.position = 'absolute';
+                    __tempEl__.style.top = (el.offsetTop + 7) + 'px';
+                    __tempEl__.style.left = el.offsetLeft + 'px';
+                    __tempEl__.style.height = 0;
+                    __tempEl__.style.opacity = 0;
+                    // Put this temp element as a child of the page <body> and focus on it
+                    document.body.appendChild(__tempEl__);
+                    __tempEl__.focus();
+
+                    // The keyboard is open. Now do a delayed focus on the target element
+                    setTimeout(function() {
+                        el.focus();
+                        el.click();
+                        // Remove the temp element
+                        document.body.removeChild(__tempEl__);
+                    }, timeout);
+                }
             }
 
-            openIosKeyboard(costField)
+
+            focusAndOpenKeyboard(costField, 1000)
         </script>
     @endscript
 </div>
